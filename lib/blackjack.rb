5da1f4d8ff5818dd12f185a5d4f27f341f8a4a41
Michelle M <michellemoon236@gmail.com>
2019-03-19 03:32:08 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -23,13 +23,20 @@ def get_user_input
   gets.chomp
 end
 
+<<<<<<< HEAD
 def end_game (final_total)
   # code #end_game here
   puts "Sorry, you hit #{final_total}. Thanks for playing!"
+=======
+def end_game (card_total)
+  # code #end_game here
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
+>>>>>>> 488d1df537959a1555e65561da04332cf5b86d09
 end
 
 def initial_round
   # code #initial_round here
+<<<<<<< HEAD
   initial_total = deal_card + deal_card
   display_card_total(initial_total)
   initial_total
@@ -42,12 +49,31 @@ def hit?(current_card_total)
     current_card_total
   elsif input == "h"
     current_card_total = current_card_total + deal_card
+=======
+  card_total = deal_card + deal_card
+  display_card_total(card_total)
+  card_total
+end
+
+def hit?(card_total)
+  # code hit? here
+  prompt_user
+  input = get_user_input
+  if input =="s"
+    card_total
+  elsif input == "h"
+    card_total = card_total + deal_card
+>>>>>>> 488d1df537959a1555e65561da04332cf5b86d09
   else
     invalid_command
 end
 end
 
 def invalid_command
+<<<<<<< HEAD
+=======
+  # code invalid_command here
+>>>>>>> 488d1df537959a1555e65561da04332cf5b86d09
   puts "Please enter a valid command"
   prompt_user
 end
@@ -57,6 +83,7 @@ end
 #####################################################
 
 def runner
+<<<<<<< HEAD
   welcome
   number = initial_round
   while number <= 21
@@ -64,6 +91,17 @@ def runner
     display_card_total (number)
   end
   end_game (number)
+=======
+  # code runner here
+  welcome
+  card_total = 0
+  initial_round
+  until card_total > 21
+    hit?
+    display_card_total
+  end
+  end_game
+>>>>>>> 488d1df537959a1555e65561da04332cf5b86d09
 
 end
     
